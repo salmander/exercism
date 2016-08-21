93728bffafbccf98d3bf3d31ad9dbfaceab25cbf
salmander <salman.great@gmail.com>
2016-08-21 23:42:34 +0100

    partial solution

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -10,17 +10,35 @@ const testVersion = 4
 type Clock struct {
 	Hour   int
 	Minute int
-} // Complete the type definition.  Pick a suitable data type.
+}
 
 func New(hour, minute int) Clock {
-	return Clock{hour, minute}
+	// Add the minutes to the hour
+	hour += (minute / 60)
+
+	// Wrap around
+	hour = hour % 24
+	minute = minute % 60
+
+	//fmt.Println("hour:", hour, " minute:", minute)
+
+	return Clock{hour % 24, minute}
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%v:%v", c.Hour, c.Minute)
+	return fmt.Sprintf("%02d:%02d", c.Hour, c.Minute)
 }
 
 func (c Clock) Add(minutes int) Clock {
-	c.Minute += minutes
+	// Check if the current time's minute and the minutes is greater than 59
+	if c.Minute+minutes > 59 {
+		//  Increment the hour
+		c.Hour++
+
+		c.Minute = minutes + c.Minute - 60
+	} else {
+		c.Minute += minutes
+	}
+
 	return c
 }
