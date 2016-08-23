ed843a899c884036c4d9ef03ac4a399cb81d1d6a
salmander <salman.great@gmail.com>
2016-08-23 20:06:24 +0100

    exercise 5 complete solution

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,54 +1,44 @@
 package raindrops
 
-import "fmt"
+import "strconv"
 
 const testVersion = 2
 
+var numbersToTest = []int{3, 5, 7}
+
 func Convert(number int) string {
-	p := fmt.Println
-
-	var output string
-	var factors []int
-
-	factors = getFactorsFor(number)
-
-	p("Factors for number:", number, "are:", factors)
-
-	output = numberToString(factors)
-
-	p("Factors for numbers are:", output)
-
-	return output
+	return getRainDropsFor(number)
 }
-func getFactorsFor(number int) []int {
-	var factors []int
 
-	for i := 1; i <= number; i++ {
+func getRainDropsFor(number int) string {
+	var words string
+	for _, i := range numbersToTest {
 		if number%i == 0 {
-			factors = append(factors, i)
+			words += numberToString(i)
 		}
 	}
-	return factors
+
+	// Check if there is nothing in the "words" string,
+	// then return the original number
+	if words == "" {
+		words += strconv.Itoa(number)
+	}
+	return words
 }
 
-func numberToString(factors []int) string {
-	var output string
-
-	if len(factors) == 1 && factors[0] == 1 {
-		output = "1"
-	} else {
-		for _, n := range factors {
-			switch n {
-			case 3:
-				output += "Pling"
-			case 5:
-				output += "Plang"
-			case 7:
-				output += "Plong"
-			}
-		}
+func numberToString(number int) string {
+	switch number {
+	case 3:
+		return "Pling"
+	case 5:
+		return "Plang"
+	case 7:
+		return "Plong"
+	default:
+		return ""
 	}
-	return output
 }
 
 // The test program has a benchmark too.  How fast does your Convert convert?
+// go test -bench .
+// BenchmarkConvert-4       1000000              1061 ns/op
