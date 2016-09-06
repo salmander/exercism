783698ac3b519618c9c2bfe67ec7a2dc692cf082
salmander <salman.great@gmail.com>
2016-09-06 16:44:55 +0100

    completed secret handshake exercise

--- go/secret-handshake/handshake.go
+++ go/secret-handshake/handshake.go
@@ -1,34 +1,37 @@
 package secret
 
-import (
-	"fmt"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
 var p = fmt.Println
 
-var secrets = map[string]string{
-	"1000": "jump",
-	"100":  "close your eyes",
-	"10":   "double blink",
-	"1":    "wink",
-}
-
 func Handshake(number int) []string {
 	var returnString []string
-	bin := strconv.FormatInt(int64(number), 2)
-	p(number, "in binary:", bin)
 
-	// Keep running the loop until the "bin"ary is blank
-	for bin != "" {
-		// Find the handshake replacement
-		for b, str := range secrets {
-			if strings.Contains(bin, b) == true {
-				bin = strings.Replace(bin, b, "", 1)
-				returnString = append(returnString, str)
-			}
+	if number <= 0 {
+		return returnString
+	}
+
+	if number&1 != 0 {
+		returnString = append(returnString, "wink")
+	}
+
+	if number&2 != 0 {
+		returnString = append(returnString, "double blink")
+	}
+
+	if number&4 != 0 {
+		returnString = append(returnString, "close your eyes")
+	}
+
+	if number&8 != 0 {
+		returnString = append(returnString, "jump")
+	}
+
+	if number&16 != 0 {
+		for i, j := 0, len(returnString)-1; i < j; i, j = i+1, j-1 {
+			returnString[i], returnString[j] = returnString[j], returnString[i]
 		}
 	}
+
 	return returnString
 }
